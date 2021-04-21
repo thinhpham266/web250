86bf685b749e52172a8603dbbb2c5a43f88f3212
thinhpham266 <48933396+thinhpham266@users.noreply.github.com>
2021-04-21 10:46:31 -0400

    Update register.php

--- register.php
+++ register.php
@@ -5,7 +5,6 @@ function register()
             $username = $_POST['username'];
             $pwd = $_POST['pwd'];
             $formtype = $_POST['formtype'];
-            echo "<script>alert('your form type is: $formtype')</script>";
             $servername = "us-cdbr-east-03.cleardb.com";
             $database = "heroku_74b6261186c9d5e";
             $dbusername = "b796116edff4ef";
@@ -17,6 +16,7 @@ function register()
                 die("Connection failed: " . mysqli_connect_error());
             }
             echo "Connected successfully";
+            echo "<script>alert('your form type is: $formtype');</script>";
        $query = "INSERT INTO customer (username, pwd) values ('".$username."','".$pwd."')";
        mysqli_query($conn, $query);
        $conn->close();
