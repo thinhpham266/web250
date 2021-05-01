8b6bc83fb24d5b86ca0a8b3617f2a3cb357918bf
thinhpham266 <48933396+thinhpham266@users.noreply.github.com>
2021-04-30 20:35:52 -0400

    Update header.php

--- components/header.php
+++ components/header.php
@@ -3,8 +3,8 @@
   <img src="images/logo.png" class="logo"></img>
   <div class="header-right">
     <a class="active" href="tp.php">Home</a>
-    <a href="#contact">Contact</a>
-    <a href="#about">About</a>
+    <a href="#input.php">Add Customer</a>
+    <a href="database.php">Delete Customer</a>
   </div>
 </div>
 </header>
