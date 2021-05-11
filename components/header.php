96af4141651ac3fe10234d097a16f29745e34b2f
thinhpham266 <48933396+thinhpham266@users.noreply.github.com>
2021-05-11 19:19:39 -0400

    Update header.php

--- components/header.php
+++ components/header.php
@@ -7,6 +7,7 @@
     <a href="https://thinhphamweb250.herokuapp.com/registerloginform.php?formtype=login">Login</a>
     <a href="input.php">Create Customer/ Search Customer's information</a>
     <a href="database.php">Delete/Update Customer/View Database</a>
+    <a href="index.php">TOC page</a>
   </div>
 </div>
 </header>
